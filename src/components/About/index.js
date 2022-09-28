6c74cf4aed817aa1b5014e919f295470e59370b8
Justin Vititoe <jvit1985@yahoo.com>
2022-09-28 13:58:33 -0500

    updated about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,7 +13,7 @@ function About() {
             <p>I have been in financial sales for over 10 years and am looking forward to transitioning to a career as a full stack software developer. I am used to dealing with client demands and deadlines from prior roles in sales and collections and I will bring the same customer centric approach to my next company. I work extremely efficiently under time constraints in a structured, fast paced environment.</p>
             <br></br>
 
-            <p>In my personal life, I have been married since May 11th, 2013. I have 2 daughters, a 6 year old and a 1 week old. My hobbies include personal finance, below average golf, and being a movie and television connoisseur.</p>
+            <p>In my personal life, I have been married since May 11th, 2013. I have 2 daughters, a 6 year old and a 3 month old. My hobbies include personal finance, below average golf, and being a movie and television connoisseur.</p>
         </div>
         </section>
     )
